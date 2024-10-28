2b2257bb05d1465c3810a4f79e927f6632f2deca
Kwon <kwonline21@gmail.com>
2024-10-28 14:41:39 +0900

    init

---
+++ src/server.js
@@ -0,0 +1,32 @@
+import net from 'net';
+import initServer from './init/index.js';
+
+const PORT = 5555;
+
+const server = net.createServer((socket) => {
+  console.log(`Client connected from: ${socket.remoteAddress}: ${socket.remotePort}`);
+
+  socket.on('data', (data) => {
+    console.log(data);
+  });
+
+  socket.on('end', () => {
+    console.log('Client disconnected');
+  });
+
+  socket.on('error', (err) => {
+    console.error('Socket error:', err);
+  });
+});
+
+initServer()
+  .then(() => {
+    server.listen(PORT, () => {
+      console.log(`Server listening on port ${PORT}`);
+      console.log(server.address());
+    });
+  })
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  });
